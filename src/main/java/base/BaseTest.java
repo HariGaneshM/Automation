ad883c3b6f5e80fbe9edfca0a73ece95b9dd5322
Hari Ganesh <hariganeshmalla29@gmail.com>
2024-12-24 17:30:33 +0530

    Initial BaseTest setup

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,5 +1,36 @@
 package base;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
+public class BaseTest {
+	
+	private ThreadLocal<WebDriver> threadsafeDriver = new ThreadLocal<WebDriver>();
+	
+	@BeforeMethod
+	public void setDriver() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		threadsafeDriver.set(driver);
+	}
+	
+	public WebDriver getDriver() {
+		return threadsafeDriver.get();
+	}
+	
+	public void goTo(String url) {
+		getDriver().get(url);
+	}
+	
+	public void navigateTo(String url) {
+		getDriver().navigate().to(url);
+	}
+	
+	@AfterMethod
+	public void closeDriver() {
+		getDriver().quit();
+		threadsafeDriver.remove();
+	}
 }
