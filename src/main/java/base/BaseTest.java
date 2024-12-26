1a9b896b75be07f5f99682e441abac313eb8d038
Hari Ganesh <hariganeshmalla29@gmail.com>
2024-12-26 14:06:38 +0530

    Driver initialization based on browser property

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,16 +2,35 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import utilities.ConfigReader;
 
 public class BaseTest {
 	
 	private ThreadLocal<WebDriver> threadsafeDriver = new ThreadLocal<WebDriver>();
 	
+	@BeforeSuite
+	public void readPropertiesFile() {
+		ConfigReader.readConfigs();
+	}
+	
 	@BeforeMethod
 	public void setDriver() {
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = null;
+		if (ConfigReader.getProperty("browser").equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if (ConfigReader.getProperty("browser").equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+		else if (ConfigReader.getProperty("browser").equalsIgnoreCase("safari")) {
+			driver = new SafariDriver();
+		}
 		driver.manage().window().maximize();
 		threadsafeDriver.set(driver);
 	}
